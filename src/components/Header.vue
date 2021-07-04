c712af9b862a10992d49013292530f7e1c4682d5
Guru Kishore <guru.kishore@wylo.in>
2021-07-04 11:47:53 +0530

    Covid tracker in vuejs

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses" /> Covid-19 tracker
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBox :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      @click="clearCountryData()"
+      v-if="stats.Country"
+      class="bg-blue-400 text-white rounded p-3 mt-10 focus:outline-none hover:bg-blue-300"
+    >
+      Clear Country
+    </button>
+  </main>
+  <main class="flex flex-col algin-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="loading_img" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTitle from '@/components/DataTitle';
+import DataBox from '@/components/DataBox';
+import CountrySelect from '@/components/CountrySelect';
 
 export default {
   name: 'Home',
+  //state
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif'),
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    DataTitle,
+    DataBox,
+    CountrySelect,
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      console.log(data);
+      return data;
+    },
+
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      this.stats = data.Global;
+      this.loading = false;
+    },
+  },
+  //life cycle methods
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
 </script>
