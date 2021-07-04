0c0ec0a131492d43aa886b346fafd258be8151f3
Guru Kishore <guru.kishore@wylo.in>
2021-07-04 12:40:25 +0530

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about flex flex-col algin-center justify-center text-center">
     <h1 class="mt-12 mb-6">
-      This is 🦠 Covid Tracker build using
-      <a class="text-gray-600" href="https://vuejs.org/">Vue js</a> +
+      This is 🦠 Covid tracker developed
+      <a class="text-gray-600" href="https://vuejs.org/">Vue js</a> and
       <a class="text-gray-600" href="https://covid19api.com/">Covid19 API</a>
     </h1>
   </div>
